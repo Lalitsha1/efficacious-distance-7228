7ed5d95c479a57aea9ca3472ecd388e0b5632c76
Lalitsha1 <115461577+Lalitsha1@users.noreply.github.com>
2022-12-14 23:43:24 +0530

    day1

---
+++ day1.js
@@ -0,0 +1,23 @@
+let div5 = document.getElementById("div5");
+
+let childdiv5 =document.createElement("div");
+
+let textp = document.createElement("p");
+textp.innerText = "ENDS TODAY!";
+textp.setAttribute  ("class" , "textp") ;
+
+
+childdiv5.append(textp);
+div5.append(childdiv5);
+// ===========================================================================================================================
+
+let div8 = document.getElementById("div8");
+
+let childdiv8 = document.createElement("div");
+
+let image = document.createElement("img");
+image.innerText = "https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dwd1c59e9e/images/Christmas2022/cndl_holtrads-wb_c1_hm.gif?yocs=s_";
+
+
+childdiv8.append(image);
+div8.append(childdiv8);
